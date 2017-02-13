5027108385b6431b4115a2247764dfdf2e081ce9
Nablaquabla <nablaquabla@live.com>
2017-02-13 12:20:33 -0500

    Changed compile script

--- compile-csi-analysis.sh
+++ compile-csi-analysis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++0x csi-analysis.cpp -lzip -L ~/usr/bin/libzip/lib/ -I ~/usr/bin/libzip/include/ -I ~/usr/bin/libzip/lib/libzip/include/ -o sns-analysis-v6
+g++ -std=c++0x csi-analysis.cpp -lzip -L ~/usr/bin/libzip/lib/ -I ~/usr/bin/libzip/include/ -I ~/usr/bin/libzip/lib/libzip/include/ -o csi-analysis
