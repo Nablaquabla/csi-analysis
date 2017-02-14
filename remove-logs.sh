7d3d06a3a42bab36a2b0144fa2bc795d0cbd23e3
Nablaquabla <nablaquabla@live.com>
2017-02-14 12:11:21 -0600

    Renamed files to match my convention

---
+++ remove-logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf /home/bjs66/Errs
+rm -rf /home/bjs66/Logs
+rm -rf /home/bjs66/Outs
+rm -rf /home/bjs66/CondorFiles
+mkdir /home/bjs66/Errs
+mkdir /home/bjs66/Logs
+mkdir /home/bjs66/Outs
+mkdir /home/bjs66/CondorFiles
