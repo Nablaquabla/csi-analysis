c5918b8d9140d081d5714982a376fe2e4c8e2714
Nablaquabla <nablaquabla@live.com>
2017-02-14 17:19:31 -0500

    Changed runs

--- condorConvertDataToHDF5.py
+++ condorConvertDataToHDF5.py
@@ -42,7 +42,8 @@ def createCondorFile(run,day):
 def main():
 
     # SNS analysis
-    runDirs = ['Run-17-02-08-16-39-02']
+    runDirs = ['Run-17-02-08-00-00-00']
+#    runDirs = ['Run-17-02-08-16-39-02']
 #    runDirs = ['Run-15-09-21-20-58-01']
 #    runDirs = ['Run-15-06-25-12-53-44']
 #    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14']
