dc7576b0c2d4e0c424cac4911bc3da9e2f2d7ae7
Nablaquabla <nablaquabla@live.com>
2017-03-01 15:16:12 -0500

    Fixed syntax for extracting stability data

--- extractStabilityData.py
+++ extractStabilityData.py
@@ -18,9 +18,13 @@ def main(argv):
     for d in h5Days:
         f = h5py.File(mainDir + run + '/' + d, 'r')
 
-        h5key = '/speq-fits/%s/%s/'%(run,d)
-        if h5key in f:
-            del f[h5key]
+        h5key = '/speq-fits/%s/%s/'%(run,d[:6])
+        if h5key in h5Stability:
+            del h5Stability[h5key + 'time']
+            for aT in ['vanilla','lbl','cmf']:
+                for t in ['fit','error']:
+                    del h5Stability[h5key + '%s/%s'%(aT,t)]
+
         h5Stability.create_dataset(h5key + 'time', data=f['/SPEQ/vanilla/Times'][...])
         h5Stability.create_dataset(h5key + 'vanilla/fit', data=f['/SPEQ/vanilla/PolyaBest'][...])
         h5Stability.create_dataset(h5key + 'vanilla/error', data=f['/SPEQ/vanilla/PolyaErr'][...])
@@ -36,7 +40,16 @@ def main(argv):
 # ============================================================================
 if __name__ == '__main__':
     mDir = '/home/bjs66/csi/bjs-analysis/'
-    runDirs = ['Run-15-06-25-12-53-44']
+    runDirs = ['Run-15-06-25-12-53-44','Run-15-06-26-11-23-13','Run-15-07-31-18-30-14',
+               'Run-16-02-15-13-46-34','Run-16-02-29-11-54-20','Run-16-03-09-13-00-14',
+               'Run-16-03-22-18-09-33','Run-16-03-30-12-44-57','Run-16-04-12-11-54-27',
+               'Run-16-04-20-11-22-48','Run-16-05-05-14-08-52','Run-16-05-12-14-07-59',
+               'Run-16-05-17-14-40-34','Run-16-06-02-12-35-56','Run-16-06-17-12-09-12',
+               'Run-16-06-27-17-50-08','Run-16-07-06-18-25-19','Run-16-07-12-11-44-55',
+               'Run-16-07-18-11-50-24','Run-16-07-21-11-59-39','Run-16-07-28-12-49-17',
+               'Run-16-08-04-17-23-52','Run-16-08-09-00-29-54','Run-16-08-16-00-22-26']
+ 
+#    runDirs = ['Run-15-06-25-12-53-44']
 #    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14']
 #    runDirs = ['Run-15-08-18-14-51-18','Run-15-08-31-00-23-36','Run-15-09-21-20-58-01']
 #    runDirs = ['Run-15-09-23-21-16-00','Run-15-10-03-09-26-22','Run-15-10-13-13-27-09']
