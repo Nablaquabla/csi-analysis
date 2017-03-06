16214bab6e72747a2e9d12a50d888600f43953c2
Nablaquabla <nablaquabla@live.com>
2017-03-06 14:40:18 -0600

    Compactify data is now callable from condor

--- _compactifyData.py
+++ _compactifyData.py
@@ -3,6 +3,7 @@
 import h5py
 import numpy as np
 import os
+import sys
 
 dataKeys = ['timestamp','median-csi-baseline','average-csi-baseline','std-csi-baseline',
             'vanilla-pt-peaks','vanilla-roi-peaks','vanilla-iw-peaks','vanilla-arrival-index','vanilla-charge','vanilla-rt-10','vanilla-rt-50','vanilla-rt-90',
@@ -14,7 +15,7 @@ def main(argv):
     run = argv[2]
 
     # Create/open stability HDF5 file that contains all stability data
-    h5Out = h5py.File(mainDir + '/Metadata/Final-Data.h5','w')
+    h5Out = h5py.File(mainDir + '/Metadata/%s-Final-Data.h5'%run,'w')
 
     # Determine all days in run folder that need to be analyzed
     h5Days = [x for x in np.sort(os.listdir(mainDir + run)) if '.h5' in x]
@@ -31,7 +32,7 @@ def main(argv):
         timeBins = h5In['/SPEQ/vanilla/Times'][...]
         timeBinsHumanReadable = np.sort(h5In['/I'].keys())
         for i in range(len(timeBins)):
-            currentTimeGroup = h5Out.create_group('/%s/%s/%s'%(run,d,timeBinsHumanReadable[i]))
+            currentTimeGroup = h5Out.create_group('/%s/%s'%(d,timeBinsHumanReadable[i]))
             for analysisType in ['vanilla','lbl','cmf']:
                 currentTimeGroup.attrs['%s-spe-charge'%analysisType] = h5In['/SPEQ/%s/PolyaBest'%analysisType][i,0]
 
@@ -82,7 +83,7 @@ def main(argv):
                     numberOfTriggers['bad-power'][qIdx] += 1
 
             for i in range(len(timeBins)):
-                currentTimeGroup = h5Out['/%s/%s/%s'%(run,d,timeBinsHumanReadable[i])]
+                currentTimeGroup = h5Out['/%s/%s'%(d,timeBinsHumanReadable[i])]
                 currentWindowGroup = currentTimeGroup.create_group('%s'%wd)
                 currentWindowGroup.attrs.create('triggers-with-power', numberOfTriggers['with-power'][i], dtype=np.uint32)
                 currentWindowGroup.attrs.create('triggers-without-power', numberOfTriggers['without-power'][i], dtype=np.uint32)
@@ -113,31 +114,5 @@ def main(argv):
 #                                Run program
 # ============================================================================
 if __name__ == '__main__':
-    mDir = '/home/bjs66/csi/bjs-analysis/'
-
-#    runDirs = ['Run-15-06-25-12-53-44','Run-15-06-26-11-23-13','Run-15-07-31-18-30-14',
-#               'Run-16-02-15-13-46-34','Run-16-02-29-11-54-20','Run-16-03-09-13-00-14',
-#               'Run-16-03-22-18-09-33','Run-16-03-30-12-44-57','Run-16-04-12-11-54-27',
-#               'Run-16-04-20-11-22-48','Run-16-05-05-14-08-52','Run-16-05-12-14-07-59',
-#               'Run-16-05-17-14-40-34','Run-16-06-02-12-35-56','Run-16-06-17-12-09-12',
-#               'Run-16-06-27-17-50-08','Run-16-07-06-18-25-19','Run-16-07-12-11-44-55',
-#               'Run-16-07-18-11-50-24','Run-16-07-21-11-59-39','Run-16-07-28-12-49-17',
-#               'Run-16-08-04-17-23-52','Run-16-08-09-00-29-54','Run-16-08-16-00-22-26']
-
-    runDirs = ['Run-15-06-25-12-53-44']
-#    runDirs = ['Run-15-06-26-11-23-13','Run-15-07-31-18-30-14']
-#    runDirs = ['Run-15-08-18-14-51-18','Run-15-08-31-00-23-36','Run-15-09-21-20-58-01']
-#    runDirs = ['Run-15-09-23-21-16-00','Run-15-10-03-09-26-22','Run-15-10-13-13-27-09']
-#    runDirs = ['Run-15-10-21-13-12-27','Run-15-10-29-15-56-36','Run-15-11-09-11-30-13']
-#    runDirs = ['Run-15-11-20-11-34-48','Run-15-11-24-15-35-32','Run-15-12-14-11-21-45']
-#    runDirs = ['Run-15-12-26-08-30-40','Run-16-01-07-12-16-36','Run-16-02-02-16-26-26']
-#    runDirs = ['Run-16-02-15-13-46-34','Run-16-02-29-11-54-20','Run-16-03-09-13-00-14']
-#    runDirs = ['Run-16-03-22-18-09-33','Run-16-03-30-12-44-57','Run-16-04-12-11-54-27']
-#    runDirs = ['Run-16-04-20-11-22-48','Run-16-05-05-14-08-52','Run-16-05-12-14-07-59']
-#    runDirs = ['Run-16-05-17-14-40-34','Run-16-06-02-12-35-56','Run-16-06-17-12-09-12']
-#    runDirs = ['Run-16-06-27-17-50-08','Run-16-07-06-18-25-19','Run-16-07-12-11-44-55']
-#    runDirs = ['Run-16-07-18-11-50-24','Run-16-07-21-11-59-39','Run-16-07-28-12-49-17']
-#    runDirs = ['Run-16-01-07-12-16-36']
-#    runDirs = ['Run-16-08-04-17-23-52','Run-16-08-09-00-29-54','Run-16-08-16-00-22-26']
     for run in runDirs:
-        main(['',mDir,run])
+        main(sys.argv)
---
+++ condorCompactifyData.py
@@ -0,0 +1,92 @@
+#!/home/bjs66/anaconda2/bin/python2.7
+
+import os
+import time as tm
+
+# Handles the creation of condor files for a given set of directories
+# -----------------------------------------------------------------------------
+def createCondorFile(run):
+    # Condor submission file name convention: run-day-time.condor
+    with open('/home/bjs66/CondorFiles/Compactify-%s.condor'%(run),'w') as f:
+
+        # Define dirs used on Condor
+        mainDataDir = '/home/bjs66/csi/bjs-analysis/'
+
+        # Fixed program location'
+        f.write('Executable = /home/bjs66/GitHub/csi-analysis/_compactifyData.py\n') #/home/bjs66/anaconda2/bin/python2.7\n')
+
+        # Arguments passed to the exe:
+        # Set main run directory, e.g. Run-15-10-02-27-32-23/151002
+	    # Set current time to be analzyed (w/o .zip extension!), e.g. 184502
+	    # Set output directory, eg Output/ Run-15-10-02-27-32-23/151002
+        f.write('Arguments = \"%s %s\"\n'%(mainDataDir,run))
+
+        # Standard cluster universe
+        f.write('universe   = vanilla\n')
+        f.write('getenv     = true\n')
+
+        # Program needs at least 300 MB of free memory to hold unzipped data
+        f.write('request_memory = 600\n')
+
+        # Output, error and log name convention: run-day-time.log/out/err
+        f.write('log = ../../Logs/CompactifyData-%s.log\n'%(run))
+        f.write('Output = ../../Outs/CompactifyData-%s.out\n'%(run))
+        f.write('Error = ../../Errs/CompactifyData-%s.err\n'%(run))
+
+        # Do not write any emails
+        f.write('notification = never\n')
+        f.write('+Department  = Physics\n')
+        f.write('should_transfer_files = NO\n')
+
+        # Add single job to queue
+        f.write('Queue')
+
+# Main function handling all internals
+# -----------------------------------------------------------------------------
+def main():
+    # Choose run to analyze
+#    runDirs = ['Run-15-06-25-12-53-44','Run-15-06-26-11-23-13','Run-15-07-31-18-30-14',
+#               'Run-16-02-15-13-46-34','Run-16-02-29-11-54-20','Run-16-03-09-13-00-14',
+#               'Run-16-03-22-18-09-33','Run-16-03-30-12-44-57','Run-16-04-12-11-54-27',
+#               'Run-16-04-20-11-22-48','Run-16-05-05-14-08-52','Run-16-05-12-14-07-59',
+#               'Run-16-05-17-14-40-34','Run-16-06-02-12-35-56','Run-16-06-17-12-09-12',
+#               'Run-16-06-27-17-50-08','Run-16-07-06-18-25-19','Run-16-07-12-11-44-55',
+#               'Run-16-07-18-11-50-24','Run-16-07-21-11-59-39','Run-16-07-28-12-49-17',
+#               'Run-16-08-04-17-23-52','Run-16-08-09-00-29-54','Run-16-08-16-00-22-26']
+    runDirs = ['Run-15-06-25-12-53-44']
+
+    for run in runDirs:
+        createCondorFile(run)
+        cmd = 'condor_submit /home/bjs66/CondorFiles/Compactify-%s.condor'%(run)
+        os.system(cmd)
+        tm.sleep(1)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
