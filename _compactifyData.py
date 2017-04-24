5666bdc801eed5f067083bec6fd680b4e98ea81a
Nablaquabla <nablaquabla@live.com>
2017-04-24 11:53:42 -0500

    Recording number of triggers and beamPower delivered:

--- _compactifyData.py
+++ _compactifyData.py
@@ -13,7 +13,7 @@ dataKeys = ['timestamp','median-csi-baseline','average-csi-baseline','std-csi-ba
 def main(argv):
     mainDir = argv[1]
     run = argv[2]
-
+        
     # Define timezones used in analysis
     eastern = pytz.timezone('US/Eastern')
     utc = pytz.utc
@@ -21,13 +21,10 @@ def main(argv):
 
     # Read times that shall be excluded from the analysis
     excludeTimes = np.loadtxt(excludeTimeFile,dtype=str)
-
     exTS = []
     for t in excludeTimes:
         exTS.append([(eastern.localize(datetime.datetime.strptime(x,'%Y-%m-%d-%H-%M-%S')).astimezone(utc)-epochBeginning).total_seconds() for x in t])
 
-
- 
     # Create/open stability HDF5 file that contains all stability data
     h5Out = h5py.File(mainDir + '/Compactified/%s-Compactified.h5'%run,'w')
 
@@ -36,11 +33,36 @@ def main(argv):
 
     # For each day get the times and the corresponding SPEQ fits
     for day in h5Days:
+    
+        # Setup new info dict that contains how many triggers have been analyzed per day and how much power has been accumulated
+        infoData = {}
+        for wd in ['S','B']:
+            infoData[wd] = {'bad-triggers': 0, 'on-triggers': 0, 'off-triggers': 0, 'power': 0, }
+        
         d = day.split('.')[0]
         h5In = h5py.File(os.path.join(mainDir,run,day),'r')
         speqCharge = h5In['/SPEQ/lbl/PolyaBest'][...][:,0]
 
         for wd in ['S','B']:
+        
+            # Get info data for waveforms without any event
+            noEventTimestamps = h5In['/%s/no-event'%wd][...]
+            noEventPower = h5In['/%s/no-event-beam-power'%wd][...]
+            timeExclusionCut = np.zeros(len(noEventTimestamps))
+            for t in exTS:
+                timeExclusionCut = timeExclusionCut + np.asarray((noEventTimestamps >= t[0]) * (noEventTimestamps <= t[1]),dtype=int)
+            timeExclusionCut = np.logical_not(np.asarray(timeExclusionCut,dtype=bool))
+
+            currentPower = noEventPower[timeExclusionCut]
+            beamOn = (currentPower > 10)
+            beamOff = (currentPower <= 10) * (currentPower >= -0.1)
+            beamBad = (currentPower == -1)
+            infoData[wd]['on-triggers'] = infoData[wd]['on-triggers'] + np.sum(beamOn)
+            infoData[wd]['off-triggers'] = infoData[wd]['off-triggers'] + np.sum(beamOff)
+            infoData[wd]['bad-triggers'] = infoData[wd]['bad-triggers'] + np.sum(beamBad)
+            infoData[wd]['power'] = infoData[wd]['power'] + np.sum(currentPower[beamOn])/1.0e6/3600.0/60.0
+               
+            
             peaksInIW = h5In['/%s/vanilla-iw-peaks'%wd][...]
             peaksInPT = h5In['/%s/vanilla-pt-peaks'%wd][...]
             rt1090 = h5In['/%s/lbl-rt-90'%wd][...] - h5In['/%s/lbl-rt-10'%wd][...]
@@ -52,19 +74,41 @@ def main(argv):
             power = h5In['/%s/beam-power'%wd][...]
             overflow = h5In['/%s/overflow-flag'%wd][...]
             muonVeto = h5In['/%s/muon-veto-flag'%wd][...]
-            linearGate = h5In['/%s/linear-gate-flag'%wd][...]
-            
+            linearGate = h5In['/%s/linear-gate-flag'%wd][...]      
             NPE[np.isnan(NPE)] = -1
 
-            cutIW = np.logical_or((peaksInIW >= 6),(NPE > 30))
-            cutPT = peaksInPT <= 10
-            cutPower = power > -0.9
-            cutLinGate = linearGate == 0
-            cutMuonVeto = muonVeto == 0
-            cutNAN = NPE >= 0
+            # Remove data within any excluded time window
+            timeExclusionCut = np.zeros(len(timestamps))
+            for t in exTS:
+                timeExclusionCut = timeExclusionCut + np.asarray((timestamps >= t[0]) * (timestamps <= t[1]),dtype=int)
+            timeExclusionCut = np.logical_not(np.asarray(timeExclusionCut,dtype=bool))
 
-            cutTotal = cutIW * cutPT * cutPower * cutLinGate * cutMuonVeto * cutNAN
+            if np.sum(timeExclusionCut) != 0:  
+                currentPower = power[timeExclusionCut]
+                beamOn = (currentPower > 10)
+                beamOff = (currentPower <= 10) * (currentPower >= -0.1)
+                beamBad = (currentPower == -1)
 
+                infoData[wd]['on-triggers'] = infoData[wd]['on-triggers'] + np.sum(beamOn)
+                infoData[wd]['off-triggers'] = infoData[wd]['off-triggers'] + np.sum(beamOff)
+                infoData[wd]['bad-triggers'] = infoData[wd]['bad-triggers'] + np.sum(beamBad)
+
+                infoData[wd]['power'] = infoData[wd]['power'] + np.sum(currentPower[beamOn])/1.0e6/3600.0/60.0
+                        
+                cutIW = np.logical_or((peaksInIW >= 6),(NPE > 30))
+                cutPT = peaksInPT <= 10
+                cutPower = power > -0.9
+                cutLinGate = linearGate == 0
+                cutMuonVeto = muonVeto == 0
+                cutNAN = NPE >= 0
+                cutTotal = cutIW * cutPT * cutPower * cutLinGate * cutMuonVeto * cutNAN * timeExclusionCut
+
+            else:
+                cutTotal = timeExclusionCut
+                
+            for key in infoData[wd].keys():
+                fOut.attrs['/%s/%s/%s'(d,wd,key)] = infoData[wd][key]
+                
             h5Out.create_dataset('/%s/%s/peaks-in-pt'%(d,wd), data=peaksInPT[cutTotal])
             h5Out.create_dataset('/%s/%s/peaks-in-iw'%(d,wd), data=peaksInIW[cutTotal])
             h5Out.create_dataset('/%s/%s/rt1090'%(d,wd), data=rt1090[cutTotal])
